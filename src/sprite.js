15805c450ebb2b293edf57884ff3d742ab738d84
benwilliamgraham <benwilliamgraham@gmail.com>
2023-11-22 17:08:20 -0500

    Sprite setup

---
+++ src/sprite.js
@@ -0,0 +1,13 @@
+"use strict";
+
+class Sprite {
+    constructor(width, height, x, y, rot) {
+        this.width = width;
+        this.height = height;
+        this.x = x;
+        this.y = y;
+        this.rot = rot;
+    }
+};
+
+export default Sprite;
