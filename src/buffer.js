e7b9fa54db9b4c521ac8e106f30f82711c8df17d
benwilliamgraham <benwilliamgraham@gmail.com>
2023-11-22 16:21:45 -0500

    Implemented buffer

--- src/buffer.js
+++ src/buffer.js
@@ -1,1 +1,17 @@
 "use strict";
+
+class Buffer {
+  constructor(gl, vertices, drawType = gl.STATIC_DRAW) {
+    this.vertexBuffer = gl.createBuffer();
+    this.update(gl, vertices);
+  }
+
+  update(gl, vertices) {
+    gl.bindBuffer(gl.ARRAY_BUFFER, this.vertexBuffer);
+    gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(vertices), gl.STATIC_DRAW);
+
+    this.numVertices = vertices.length / 2;
+  }
+}
+
+export default Buffer;
