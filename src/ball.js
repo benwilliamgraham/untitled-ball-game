37dd8c036d3cb436de311d23e1792626e09dc669
benwilliamgraham <benwilliamgraham@gmail.com>
2023-12-01 16:28:42 -0800

    Update ball size and physics

--- src/ball.js
+++ src/ball.js
@@ -4,7 +4,8 @@ import Sprite from "./sprite.js"
 
 class Ball extends Sprite {
     constructor(radius, x, y, rot, texture) {
-        super(radius, radius, x, y, rot, texture);
+        super(radius * 2, radius * 2, x, y, rot, texture);
+        this.radius = radius;
 
         this.velX = 0;
         this.velY = 0;
--- src/game.js
+++ src/game.js
@@ -1,9 +1,9 @@
 "use strict";
 
-import Renderer from "./renderer.js"
-import Scene from "./scene.js"
-import Sprite from "./sprite.js"
-import Ball from "./ball.js"
+import Renderer from "./renderer.js";
+import Scene from "./scene.js";
+import Sprite from "./sprite.js";
+import Ball from "./ball.js";
 
 class Game {
   constructor(canvas, renderer) {
@@ -14,7 +14,15 @@ class Game {
 
     this.balls = new Set();
 
-    this.scene.sprites.add(new Ball(100, 100, 100, 0, renderer.getTexture("src/textures/a.png")));
+    const ball = new Ball(
+      100,
+      100,
+      500,
+      0,
+      renderer.getTexture("src/textures/a.png")
+    );
+    this.balls.add(ball);
+    this.scene.sprites.add(ball);
   }
 
   static async init(canvas) {
@@ -23,14 +31,47 @@ class Game {
   }
 
   play() {
-    const {renderer, scene} = this;
+    const { canvas, renderer, scene, balls} = this;
 
-    let lastTime = 0;
+      function updatePhysics(dt) {
+        for (const ball of balls) {
+          ball.velY -= 0.001 * dt;
+    
+          ball.x += ball.velX * dt;
+          ball.y += ball.velY * dt;
+        }
+        const dampening = 0.7;
+        for (const ball of balls) {
+          // Check if the ball has left the bounds of the screen
+          if (ball.y - ball.radius < 10) {
+            ball.y = 10 + ball.radius;
+            ball.velY = -ball.velY * dampening;
+          }
+          if (ball.x - ball.radius < 0) {
+            ball.x = 10 + ball.radius;
+            ball.velX = -ball.velX * dampening;
+          }
+          if (ball.x + ball.radius > canvas.width) {
+            ball.x = canvas.width - ball.radius;
+            ball.velX = -ball.velX * dampening;
+          }
+        }
+      }
+
+      let lastTime = 0,
+      accumulator = 0;
     function gameLoop(time) {
       const dt = (time - lastTime) % 1000; // Prevents delta time from getting too large
       lastTime = time;
+      accumulator += dt;
 
       // Apply physics
+      const step = 1 / 60;
+      while (accumulator > step) {
+        updatePhysics(step);
+
+        accumulator -= step;
+      }
 
       // Render scene
       renderer.render(scene);
