50f2be191e9dc2936f3e9c5286c8292524904845
benwilliamgraham <benwilliamgraham@gmail.com>
2023-12-02 22:57:50 -0800

    Add ball physics and collision detection

--- src/ball.js
+++ src/ball.js
@@ -6,6 +6,7 @@ class Ball extends Sprite {
     constructor(radius, x, y, rot, texture) {
         super(radius * 2, radius * 2, x, y, rot, texture);
         this.radius = radius;
+        this.mass = radius * radius;
 
         this.velX = 0;
         this.velY = 0;
--- src/game.js
+++ src/game.js
@@ -13,16 +13,6 @@ class Game {
     this.scene = new Scene(renderer);
 
     this.balls = new Set();
-
-    const ball = new Ball(
-      100,
-      100,
-      500,
-      0,
-      renderer.getTexture("src/textures/a.png")
-    );
-    this.balls.add(ball);
-    this.scene.sprites.add(ball);
   }
 
   static async init(canvas) {
@@ -31,34 +21,118 @@ class Game {
   }
 
   play() {
-    const { canvas, renderer, scene, balls} = this;
+    const { canvas, renderer, scene, balls } = this;
 
-      function updatePhysics(dt) {
-        for (const ball of balls) {
-          ball.velY -= 0.001 * dt;
-    
-          ball.x += ball.velX * dt;
-          ball.y += ball.velY * dt;
+    // Setup input
+    canvas.addEventListener("click", (event) => {
+      const rect = canvas.getBoundingClientRect();
+      const x = event.clientX - rect.left;
+      const y = event.clientY - rect.top;
+
+      const ball = new Ball(
+        100,
+        x,
+        canvas.height - y,
+        0,
+        renderer.getTexture("src/textures/a.png")
+      );
+      balls.add(ball);
+      scene.sprites.add(ball);
+    }
+    );
+
+    // Create physics loop
+    function updatePhysics(dt) {
+      const restitution = 0.7;
+      
+      // Apply gravity and update position
+      for (const ball of balls) {
+        ball.velY -= 0.001 * dt;
+
+        ball.x += ball.velX * dt;
+        ball.y += ball.velY * dt;
+      }
+      
+      // Check for collisions with the floor and walls
+      for (const ball of balls) {
+        // Check if the ball has left the bounds of the screen
+        if (ball.y - ball.radius < 10) {
+          ball.y = 10 + ball.radius;
+          ball.velY = -ball.velY * restitution;
         }
-        const dampening = 0.7;
-        for (const ball of balls) {
-          // Check if the ball has left the bounds of the screen
-          if (ball.y - ball.radius < 10) {
-            ball.y = 10 + ball.radius;
-            ball.velY = -ball.velY * dampening;
-          }
-          if (ball.x - ball.radius < 0) {
-            ball.x = 10 + ball.radius;
-            ball.velX = -ball.velX * dampening;
-          }
-          if (ball.x + ball.radius > canvas.width) {
-            ball.x = canvas.width - ball.radius;
-            ball.velX = -ball.velX * dampening;
+        if (ball.x - ball.radius < 0) {
+          ball.x = ball.radius;
+          ball.velX = -ball.velX * restitution;
+        }
+        if (ball.x + ball.radius > canvas.width) {
+          ball.x = canvas.width - ball.radius;
+          ball.velX = -ball.velX * restitution;
+        }
+      }
+
+      // Create list of colliding balls
+      const collidingBalls = [];
+      const ballsArr = Array.from(balls);
+      for (let i = 0; i < balls.size; i++) {
+        const ball = ballsArr[i];
+        for (let j = i + 1; j < balls.size; j++) {
+          const otherBall = ballsArr[j];
+
+          const dx = ball.x - otherBall.x;
+          const dy = ball.y - otherBall.y;
+          const distance = Math.sqrt(dx * dx + dy * dy);
+
+          if (distance < ball.radius + otherBall.radius) {
+            collidingBalls.push([ball, otherBall]);
           }
         }
       }
 
-      let lastTime = 0,
+      // Resolve collisions
+      for (const [a, b] of collidingBalls) {
+        // Calculate the normal vector
+        const dx = b.x - a.x;
+        const dy = b.y - a.y;
+        const distance = Math.sqrt(dx * dx + dy * dy);
+        const normalX = dx / distance;
+        const normalY = dy / distance;
+
+        // Separate the balls
+        const overlap = (a.radius + b.radius) - distance;
+
+        a.x -= overlap * normalX * 0.5;
+        a.y -= overlap * normalY * 0.5;
+        b.x += overlap * normalX * 0.5;
+        b.y += overlap * normalY * 0.5;
+
+        // Calculate the relative velocity
+        const relVelX = b.velX - a.velX;
+        const relVelY = b.velY - a.velY;
+
+        // Calculate the relative velocity in terms of the normal direction
+        const normalVel = relVelX * normalX + relVelY * normalY;
+
+        // Do not resolve if velocities are separating
+        if (normalVel > 0) {
+          continue;
+        }
+
+        // Calculate impulse scalar
+        let impulseScalar = -(1 + restitution) * normalVel;
+        impulseScalar /= 1 / a.mass + 1 / b.mass;
+
+        // Apply impulse
+        const impulseX = impulseScalar * normalX;
+        const impulseY = impulseScalar * normalY;
+
+        a.velX -= 1 / a.mass * impulseX;
+        a.velY -= 1 / a.mass * impulseY;
+        b.velX += 1 / b.mass * impulseX;
+        b.velY += 1 / b.mass * impulseY;
+      }
+    }
+
+    let lastTime = 0,
       accumulator = 0;
     function gameLoop(time) {
       const dt = (time - lastTime) % 1000; // Prevents delta time from getting too large
@@ -66,7 +140,7 @@ class Game {
       accumulator += dt;
 
       // Apply physics
-      const step = 1 / 60;
+      const step = 1000 / 512;
       while (accumulator > step) {
         updatePhysics(step);
 
