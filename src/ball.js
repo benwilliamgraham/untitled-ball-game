9474482a94e867bcb3e1627637c7ca8689a137e7
benwilliamgraham <benwilliamgraham@gmail.com>
2023-12-02 23:45:02 -0800

    Refactor ball creation and collision handling

--- src/ball.js
+++ src/ball.js
@@ -2,27 +2,10 @@
 
 import Sprite from "./sprite.js"
 
-const levelHeirarchy = [
-    {radius: 10, texture: "src/textures/0.png"},
-    {radius: 20, texture: "src/textures/1.png"},
-    {radius: 30, texture: "src/textures/2.png"},
-    {radius: 40, texture: "src/textures/3.png"},
-    {radius: 50, texture: "src/textures/4.png"},
-    {radius: 60, texture: "src/textures/5.png"},
-    {radius: 70, texture: "src/textures/6.png"},
-    {radius: 80, texture: "src/textures/7.png"},
-    {radius: 90, texture: "src/textures/8.png"},
-    {radius: 100, texture: "src/textures/9.png"},
-    {radius: 110, texture: "src/textures/10.png"},
-];
-
 class Ball extends Sprite {
-    levelHeirarchy = levelHeirarchy;
 
-    constructor(level, x, y, renderer) {
-        const {radius, texture} = levelHeirarchy[level];
-        console.log(levelHeirarchy[level]);
-        super(radius * 2, radius * 2, x, y, 0, renderer.getTexture(texture));
+    constructor(level, radius, x, y, texture) {
+        super(radius * 2, radius * 2, x, y, 0, texture);
         this.radius = radius;
         this.mass = radius * radius;
         this.level = level;
--- src/game.js
+++ src/game.js
@@ -23,27 +23,43 @@ class Game {
   play() {
     const { canvas, renderer, scene, balls } = this;
 
+    // Create ball hierarchy
+    const levelHeirarchy = [
+      { radius: 20, texture: renderer.getTexture("src/textures/0.png") },
+      { radius: 25, texture: renderer.getTexture("src/textures/1.png") },
+      { radius: 30, texture: renderer.getTexture("src/textures/2.png") },
+      { radius: 35, texture: renderer.getTexture("src/textures/3.png") },
+      { radius: 40, texture: renderer.getTexture("src/textures/4.png") },
+      { radius: 45, texture: renderer.getTexture("src/textures/5.png") },
+      { radius: 50, texture: renderer.getTexture("src/textures/6.png") },
+      { radius: 55, texture: renderer.getTexture("src/textures/7.png") },
+      { radius: 60, texture: renderer.getTexture("src/textures/8.png") },
+      { radius: 65, texture: renderer.getTexture("src/textures/9.png") },
+      { radius: 70, texture: renderer.getTexture("src/textures/10.png") },
+    ];
+
     // Setup input
     canvas.addEventListener("click", (event) => {
       const rect = canvas.getBoundingClientRect();
       const x = event.clientX - rect.left;
       const y = event.clientY - rect.top;
 
+      const level = Math.floor(Math.random() * 11);
       const ball = new Ball(
-        Math.floor(Math.random() * 11),
+        level,
+        levelHeirarchy[level].radius,
         x,
         canvas.height - y,
-        renderer
+        levelHeirarchy[level].texture
       );
       balls.add(ball);
       scene.sprites.add(ball);
-    }
-    );
+    });
 
     // Create physics loop
     function updatePhysics(dt) {
       const restitution = 0.7;
-      
+
       // Apply gravity and update position
       for (const ball of balls) {
         ball.velY -= 0.001 * dt;
@@ -51,7 +67,7 @@ class Game {
         ball.x += ball.velX * dt;
         ball.y += ball.velY * dt;
       }
-      
+
       // Check for collisions with the floor and walls
       for (const ball of balls) {
         // Check if the ball has left the bounds of the screen
@@ -69,8 +85,9 @@ class Game {
         }
       }
 
-      // Create list of colliding balls
+      // Create list of colliding balls and combined balls
       const collidingBalls = [];
+      const combinedBalls = [];
       const ballsArr = Array.from(balls);
       for (let i = 0; i < balls.size; i++) {
         const ball = ballsArr[i];
@@ -82,7 +99,11 @@ class Game {
           const distance = Math.sqrt(dx * dx + dy * dy);
 
           if (distance < ball.radius + otherBall.radius) {
-            collidingBalls.push([ball, otherBall]);
+            if (ball.level === otherBall.level) {
+              combinedBalls.push([ball, otherBall]);
+            } else {
+              collidingBalls.push([ball, otherBall]);
+            }
           }
         }
       }
@@ -97,7 +118,7 @@ class Game {
         const normalY = dy / distance;
 
         // Separate the balls
-        const overlap = (a.radius + b.radius) - distance;
+        const overlap = a.radius + b.radius - distance;
 
         a.x -= overlap * normalX * 0.5;
         a.y -= overlap * normalY * 0.5;
@@ -124,10 +145,32 @@ class Game {
         const impulseX = impulseScalar * normalX;
         const impulseY = impulseScalar * normalY;
 
-        a.velX -= 1 / a.mass * impulseX;
-        a.velY -= 1 / a.mass * impulseY;
-        b.velX += 1 / b.mass * impulseX;
-        b.velY += 1 / b.mass * impulseY;
+        a.velX -= (1 / a.mass) * impulseX;
+        a.velY -= (1 / a.mass) * impulseY;
+        b.velX += (1 / b.mass) * impulseX;
+        b.velY += (1 / b.mass) * impulseY;
+      }
+
+      // Combine balls
+      for (const [a, b] of combinedBalls) {
+        const level = Math.min(a.level + 1, 10);
+        const x = (a.x + b.x) / 2;
+        const y = (a.y + b.y) / 2;
+
+        balls.delete(a);
+        balls.delete(b);
+        scene.sprites.delete(a);
+        scene.sprites.delete(b);
+
+        const ball = new Ball(
+          level,
+          levelHeirarchy[level].radius,
+          x,
+          y,
+          levelHeirarchy[level].texture
+        );
+        balls.add(ball);
+        scene.sprites.add(ball);
       }
     }
 
